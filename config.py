e6827939f67692249f67c1765223f1a3e6f755ab
Nisith Nimtharu Welivitiya <155635480+NisithNim2007@users.noreply.github.com>
2025-07-14 11:46:06 +0530

    update 13

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ SECRET_KEY = 'your_secret_key'
 MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
-MAIL_USERNAME = 'nisith.ipad@gmail.com'
-MAIL_PASSWORD = 'otra mpcv nwpt gmtc'  # Not your real Gmail password!
+MAIL_USERNAME = 'horizontravels1234@gmail.com'
+MAIL_PASSWORD = 'lvqo pyod drjn ngxc'  # Not your real Gmail password!
